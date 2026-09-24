b58a1789bda6d02def1b752e1ac3718b6a944eff
Nuri Ibañez <nuriei@hotmail.com>
2026-09-24 00:25:36 -0300

    terminado

---
+++ Guia2_TP6/src/guia2_tp6/Producto.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia2_tp6;
+
+/**
+ *
+ * @author Nuri
+ */
+public class Producto {
+    private String nombre;
+    private String categoria;
+    private double precio;
+
+    public Producto(String nombre, String categoria, double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+
+    
+    
+}
